bc345ed0eee30c81bae726a557492d600dc9e3cc
Calum J. Eadie <calum@calumjeadie.com>
2014-03-25 15:21:44 +0000

    Basic profiles

--- people/views.py
+++ people/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 def index(request):
 
-    return render(request, 'index.html')
+    people = [
+        {
+            'name': 'Calum J. Eadie',
+            'email': 'calum@calumjeadie.com',
+            'university': 'Cambridge',
+            'graduation_year': 2013,
+            'location': 'London',
+            'twitter': 'CalumJEadie',
+            'github': 'CalumJEadie',
+            'linkedin': 'calumjeadie',
+            'url': 'http://calumjeadie.com'
+        }
+    ]
+
+    print people
+
+    return render(request, 'index.html', {'people': people})
